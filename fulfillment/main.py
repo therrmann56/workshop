919c3d217cd5dac248ead259ac38af625804d653
Hendrik Mennen <hmennen@gambio.ec1.de>
2025-06-11 12:08:29 +0200

    Implemented consumer for order topic
    Listen for event status MERCHANT_ACCEPTED or SHIPPED
    
    Todo: Implement logic per status

--- fulfillment/main.py
+++ fulfillment/main.py
@@ -1,8 +1,16 @@
 # filter/main.py
 # Dieses Skript liest Logzeilen ein und filtert nur Zeilen mit dem Statuscode 200.
 
-input_path = "/data/logs.txt"
-output_path = "/data/filtered_logs.txt"
+import json
+import uuid
+from datetime import datetime
+from confluent_kafka import Consumer, KafkaException
+from sqlalchemy import create_engine, Column, String, Integer, DECIMAL, DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+
+input_path = "data/logs.txt"
+output_path = "data/filtered_logs.txt"
 
 if __name__ == "__main__":
     with open(input_path, "r") as infile, open(output_path, "w") as outfile:
@@ -10,3 +18,43 @@ if __name__ == "__main__":
             if " 200 " in line:
                 outfile.write(line)
     print(f"Gefilterte Logzeilen nach {output_path} geschrieben.")
+
+
+DB_URI = 'mysql+pymysql://user:userpw@[fd00:dead:cafe::100]:3306/analytics'
+engine = create_engine(DB_URI, echo=True)
+Session = sessionmaker(bind=engine)
+
+
+# === Kafka Consumer ===
+kafka_conf_merchant_accepted = {
+    'bootstrap.servers': '[fd00:dead:cafe::10]:9092',
+    'group.id': 'fulfillment-processing-group',
+    'auto.offset.reset': 'earliest'
+}
+
+
+consumer = Consumer(kafka_conf_merchant_accepted)
+consumer.subscribe(['order'])
+print("? Warte auf Order-Events ...")
+
+try:
+    while true:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            raise KafkaException(msg.error())
+
+        session = Session()
+
+        try:
+            payload = json.loads(msg.value().decode('utf-8'))
+            if payload.get("status") == "MERCHANT_ACCEPTED":
+                # FULFILLMENT
+            elif payload.get("status") == "SHIPPED":
+                # Completed
+
+
+except:
+
+finally:
